36f244232ade324cce9b03ea652cf747f6ba4871
williamtr4n <95212103+williamtr4n@users.noreply.github.com>
2021-12-29 10:25:23 +0700

    Add files via upload

--- js/admin.js
+++ js/admin.js
@@ -100,11 +100,14 @@ function create(e) {
             [product_name, product_description, product_img_src, product_price, product_category],
             function (tx, result) {
                 log("INFO", `Create product "${product_name}" successfully.`);
-                $("#messageCreate").modal("show");
+
                 document.getElementById("create-product-frm").reset();
                 document.getElementById("name").focus();
             },
-            transaction_error
+            function (tx, result) {
+                log(`ERROR`, `Existed product name.`);
+                $("#messageCreateFailed").modal("show");
+            }
         );
     });
 }
